01d2f2c6a73ec1a0a2ff7d2be7fdc2397b1aa541
NielsCharlier <niels@scitus.be>
2017-12-21 16:36:32 +0100

    DOVINSPIRE-2064 verbetering tests (missing file)

---
+++ src/community/taskmanager/src/main/java/org/geoserver/taskmanager/external/H2DialectImpl.java
@@ -0,0 +1,27 @@
+/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.taskmanager.external;
+
+/**
+ * Generic implementation for the Dialect interface. This should work with moest databases. But is also limits the functionality of the taskmanager;
+ */
+public class H2DialectImpl implements Dialect {
+
+    /**
+     * Do not quote table names since this not supported by all db's.
+     *
+     * @param tableName
+     * @return
+     */
+    @Override
+    public String quote(String tableName) {
+        return tableName;
+    }
+
+    @Override
+    public String sqlRenameView(String currentViewName, String newViewName) {
+        return "ALTER TABLE " + currentViewName + " RENAME TO " + newViewName;
+    }
+}
