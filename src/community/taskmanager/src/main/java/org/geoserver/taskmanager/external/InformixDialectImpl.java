314d84da3c69037ef15bd1250c6cd9767bc35b07
timothy <timothy.debock.apollo@gmail.com>
2018-01-16 15:15:56 +0100

    no quotes for informix

---
+++ src/community/taskmanager/src/main/java/org/geoserver/taskmanager/external/InformixDialectImpl.java
@@ -0,0 +1,20 @@
+/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.taskmanager.external;
+
+/**
+ * Informix implementation.
+ *
+ * 
+ * @author Timothy De Bock
+ */
+public class InformixDialectImpl extends DefaultDialectImpl {
+
+    @Override
+    public String quote(String tableName) {
+        return tableName;
+    }
+
+}
