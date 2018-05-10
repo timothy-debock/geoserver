cc07992ec84c65b824c23fe927eaf111b2d37ed5
NielsCharlier <niels@scitus.be>
2018-05-10 10:36:51 +0200

    split s3 module

--- src/community/taskmanager/src/main/java/org/geoserver/taskmanager/schedule/TaskRunnable.java
+++
@@ -1,9 +0,0 @@
-/* (c) 2017 Open Source Geospatial Foundation - all rights reserved
- * This code is licensed under the GPL 2.0 license, available at the root
- * application directory.
- */
-package org.geoserver.taskmanager.schedule;
-
-public interface TaskRunnable<T> {
-    void run(T arg) throws TaskException;
-}
