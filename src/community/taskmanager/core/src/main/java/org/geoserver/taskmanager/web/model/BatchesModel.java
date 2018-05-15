89bb2cf2a80825510bee579c0fb3a625b06ff20f
Niels Charlier <niels@scitus.be>
2018-05-15 17:43:17 +0200

    frequency sort

--- src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/web/model/BatchesModel.java
+++ src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/web/model/BatchesModel.java
@@ -6,6 +6,7 @@ package org.geoserver.taskmanager.web.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.wicket.model.IModel;
@@ -18,10 +19,40 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class BatchesModel extends GeoServerDataProvider<Batch> {
 
     private static final long serialVersionUID = -8246320435114536132L;
+    
+    public static class StringReversePropertyComparator<T> implements Comparator<T> {
+        Property<T> property;
+
+        public StringReversePropertyComparator(Property<T> property) {
+            this.property = property;
+        }
+
+        public int compare(T o1, T o2) {
+            Object p1 = property.getPropertyValue(o1);
+            Object p2 = property.getPropertyValue(o2);
+
+            // what if any property is null? We assume null < (not null)
+            if (p1 == null)
+                return p2 != null ? -1 : 0;
+            else if (p2 == null)
+                return 1;
+
+            return new StringBuffer(p1.toString()).reverse().toString().compareTo(
+                    new StringBuffer(p2.toString()).reverse().toString());
+        }
+
+    }
 
     public static final Property<Batch> WORKSPACE = new BeanProperty<Batch>("workspace", "workspace");
     public static final Property<Batch> DESCRIPTION = new BeanProperty<Batch>("description", "description");
-    public static final Property<Batch> FREQUENCY = new BeanProperty<Batch>("frequency", "frequency");
+    public static final Property<Batch> FREQUENCY = new BeanProperty<Batch>("frequency", "frequency") {        
+        private static final long serialVersionUID = -5039727601429342722L;
+
+        @Override
+        public Comparator<Batch> getComparator() {
+            return new StringReversePropertyComparator<Batch>(this);
+        }
+    };
     public static final Property<Batch> ENABLED = new BeanProperty<Batch>("enabled", "enabled");
     public static final Property<Batch> NAME = new BeanProperty<Batch>("name", "name");
     public static final Property<Batch> STARTED = new AbstractProperty<Batch>("started") {
@@ -55,6 +86,7 @@ public class BatchesModel extends GeoServerDataProvider<Batch> {
         }
         
     };
+    
     public static final Property<Batch> RUN = new AbstractProperty<Batch>("run") {
 
         private static final long serialVersionUID = -978472501994535469L;
@@ -98,6 +130,7 @@ public class BatchesModel extends GeoServerDataProvider<Batch> {
                     configurationModel.getObject().getBatches().values());
         list.removeIf(b -> !TaskManagerBeans.get().getSecUtil().isReadable(
             SecurityContextHolder.getContext().getAuthentication(), b));
+        
         return list;
     }
 
