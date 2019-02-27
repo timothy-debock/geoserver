fd87e3007ab2cd3717b3f7ef6828f06cc5cfdc3d
Niels Charlier <niels@scitus.be>
2019-02-27 12:16:11 +0100

    Mass layer fix in REST

--- src/community/metadata/src/main/java/org/geoserver/metadata/rest/MetaDataRestService.java
+++ src/community/metadata/src/main/java/org/geoserver/metadata/rest/MetaDataRestService.java
@@ -1,14 +1,20 @@
 package org.geoserver.metadata.rest;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.ResourceInfo;
+import org.geoserver.metadata.data.model.impl.ComplexMetadataMapImpl;
+import org.geoserver.metadata.data.service.ComplexMetadataService;
 import org.geoserver.metadata.data.service.MetadataTemplateService;
 import org.geoserver.metadata.data.service.impl.MetadataConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +26,8 @@ public class MetaDataRestService {
     @Autowired private Catalog catalog;
 
     @Autowired private MetadataTemplateService templateService;
+
+    @Autowired private ComplexMetadataService metadataService;
 
     @DeleteMapping
     public void clearAll(
@@ -36,5 +44,19 @@ public class MetaDataRestService {
             }
             templateService.saveList(Collections.emptyList());
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    @GetMapping("fix")
+    public String fixAll() {
+        for (ResourceInfo info : catalog.getResources(ResourceInfo.class)) {
+            Serializable custom = info.getMetadata().get(MetadataConstants.CUSTOM_METADATA_KEY);
+            if (custom instanceof HashMap<?, ?>) {
+                metadataService.init(
+                        new ComplexMetadataMapImpl((Map<String, Serializable>) custom));
+            }
+            catalog.save(info);
+        }
+        return "Success.";
     }
 }
