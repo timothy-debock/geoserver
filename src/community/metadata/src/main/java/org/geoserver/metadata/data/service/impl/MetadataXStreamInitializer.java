e0f1295e189854b88787eafc9709f208506cf6ff
Niels Charlier <niels@scitus.be>
2019-03-15 13:46:19 +0100

    DOVINSPIRE-2927 DOVINSPIRE-2842 foutmelding wms/wfs

--- src/community/metadata/src/main/java/org/geoserver/metadata/data/service/impl/MetadataXStreamInitializer.java
+++ src/community/metadata/src/main/java/org/geoserver/metadata/data/service/impl/MetadataXStreamInitializer.java
@@ -4,10 +4,7 @@
  */
 package org.geoserver.metadata.data.service.impl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.geoserver.config.util.XStreamPersister;
 import org.geoserver.config.util.XStreamPersisterInitializer;
 import org.springframework.stereotype.Component;
@@ -17,9 +14,6 @@ public class MetadataXStreamInitializer implements XStreamPersisterInitializer {
 
     @Override
     public void init(XStreamPersister persister) {
-        persister.registerBreifMapComplexType("list", ArrayList.class);
         persister.registerBreifMapComplexType("map", HashMap.class);
-        persister.getXStream().alias("list", List.class, ArrayList.class);
-        persister.getXStream().alias("map", Map.class, HashMap.class);
     }
 }
